bbcf48e134964e3386be2ca30aa9adad15bf170a
Igor Krupin <clearwaterstream@gmail.com>
2019-06-29 00:40:17 -0400

    added app sample

---
+++ samples/RestaurantTableTracker/TableStatus.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RestaurantTableTracker
+{
+    /// <summary>
+    /// Interaction logic for TableStatus.xaml
+    /// </summary>
+    public partial class TableStatus : UserControl
+    {
+        public TableStatus()
+        {
+            InitializeComponent();
+        }
+
+        public string TableName
+        {
+            get
+            {
+                return (string)lblTableName.Content;
+            }
+            set
+            {
+                lblTableName.Content = value;
+            }
+        }
+    }
+}
