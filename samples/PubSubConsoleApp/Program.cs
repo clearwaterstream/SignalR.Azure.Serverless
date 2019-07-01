bfb13a85b57950a320ad05b42fae1428c924bb38
Igor Krupin <clearwaterstream@gmail.com>
2019-07-01 00:48:41 -0400

    added console sample

---
+++ samples/PubSubConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
+
+namespace PubSubConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var configBuilder = new ConfigurationBuilder();
+
+            configBuilder.AddUserSecrets("a0aa8545-9a28-485e-8218-9851c41dcfbb");
+
+            var config = configBuilder.Build();
+
+            // copy the CONNECTION STRING value from the Azure Portal under SingalR > [item] > Keys (under Settings)
+            // then in the csproj dir, run dotnet user-secrets set "signalRConnString" "conn_string_value"
+            var connStr = config["signalRConnString"];
+
+            using (var sample = new PubSubSample(connStr))
+            {
+                await sample.Run();
+
+                await Task.Delay(3000);
+
+                await sample.Stop();
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ samples/PubSubConsoleApp/PubSubSample.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using SignalR.Azure.Serverless;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PubSubConsoleApp
+{
+    public class PubSubSample : IDisposable
+    {
+        readonly HttpClient signalRApiHttpClient = new HttpClient();
+        readonly SignalRHubHelper signalRHubHelper;
+        readonly HubConnection signalRHubConnection;
+
+        public PubSubSample(string connString)
+        {
+            signalRHubHelper = new SignalRHubHelper(connString);
+
+            var hubUrl = signalRHubHelper.ClientUrl("default_hub");
+
+            // define a connection to receive published events
+            signalRHubConnection = new HubConnectionBuilder().WithUrl(hubUrl, option =>
+            {
+                option.AccessTokenProvider = () =>
+                {
+                    var token = signalRHubHelper.GenerateAccessToken(hubUrl, "user-x");
+
+                    return Task.FromResult(token);
+                };
+            }).Build();
+
+            // define an event handler for received messages
+            signalRHubConnection.On<string, string>("StatusChanged", (deviceId, status) =>
+            {
+                Console.WriteLine($"Device {deviceId} changed status to {status}");
+            });
+        }
+
+        public async Task Run()
+        {
+            // start listening to published events
+            await signalRHubConnection.StartAsync();
+
+            Console.WriteLine("Listening for events...");
+
+            var signalRApiClient = new SignalRApiClient(signalRApiHttpClient, signalRHubHelper);
+
+            var msg = new SignalRMessage()
+            {
+                target = "StatusChanged",
+                arguments = new object[] { "device_1", "offline" }
+            };
+
+            // send a test message
+            await signalRApiClient.BroadcastToAllClients(senderUserId: "user-x", hubName: "default_hub", msg);
+        }
+
+        public async Task Stop()
+        {
+            await signalRHubConnection.DisposeAsync();
+        }
+
+        public void Dispose()
+        {
+            signalRApiHttpClient?.Dispose();
+
+            GC.SuppressFinalize(this);
+        }
+    }
+}
